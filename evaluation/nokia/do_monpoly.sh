d20f041a28805920b7c2ef157f6d09ea8282e043
Joshua Schneider <joshua.schneider@inf.ethz.ch>
2018-06-15 18:40:02 +0200

    fix evaluation script once more

--- evaluation/nokia/do_monpoly.sh
+++ evaluation/nokia/do_monpoly.sh
@@ -1,35 +1,27 @@
 #!/usr/bin/env bash
 
+set -e
+
 WORK_DIR=`cd "$(dirname "$BASH_SOURCE")/.."; pwd`
 source "$WORK_DIR/config.sh"
 
 ITERATIONS=1
-FORMULAS="script1.mfotl del-1-2.mfotl"
+FORMULAS="script1 del-1-2"
 ACCELERATION=3000
-
-REPORT_FILE1="$REPORT_DIR/nokia_monpoly1.txt"
-REPORT_FILE2="$REPORT_DIR/nokia_monpoly2.txt"
-
-if [[ -f $REPORT_FILE1 || -f $REPORT_FILE2 ]]; then
-    echo "[ERROR]: The report files already exist. Please delete them first:"
-    echo "  $REPORT_FILE1"
-    echo "  $REPORT_FILE2"
-    exit 1
-fi
 
 echo "=== NOKIA / MONPOLY ==========================================================="
 
 for formula in $FORMULAS; do
     echo "Evaluating $formula:"
-    echo "$formula" >> "$REPORT_FILE1"
-    echo "$formula" >> "$REPORT_FILE2"
     for i in $(seq 1 $ITERATIONS); do
         echo "  Iteration $i ..."
-        taskset -c $AUX_CPU_LIST "$WORK_DIR/replayer.sh" -a $ACCELERATION -m "$WORK_DIR/ldcc_sample.csv" 2>> "$REPOT_FILE1" \
-            | taskset -c $CPU_LIST $TIME_COMMAND -a -f "%e %M" -o "$REPORT_FILE2" "$WORK_DIR/monpoly" -sig "$WORK_DIR/nokia/ldcc.sig" -formula "$1" -negate > /dev/null
+
+        DELAY_REPORT="$REPORT_DIR/nokia_monpoly_$formula$_$i_delay.txt"
+        SUMMARY_REPORT="$REPORT_DIR/nokia_monpoly_$formula_$i_summary.txt"
+
+        (taskset -c $AUX_CPU_LIST "$WORK_DIR/replayer.sh" -v -a $ACCELERATION -m "$WORK_DIR/ldcc_sample.csv" 2> "$DELAY_REPORT") \
+            | taskset -c $CPU_LIST "$TIME_COMMAND" -f "%e %M" -o "$SUMMARY_REPORT" "$WORK_DIR/monpoly" -sig "$WORK_DIR/nokia/ldcc.sig" -formula "$WORK_DIR/nokia/$formula.mfotl" -negate > /dev/null
     done
-    echo >> "$REPORT_FILE1"
-    echo >> "$REPORT_FILE2"
 done
 
 echo "Evaluation complete!"
