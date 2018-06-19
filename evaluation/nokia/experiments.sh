a6514940f38b69776649522b0bf44232002ac726
Joshua Schneider <joshua.schneider@inf.ethz.ch>
2018-06-19 12:42:43 +0200

    use larger replayer queue in experiments to prevent underruns

--- evaluation/nokia/experiments.sh
+++ evaluation/nokia/experiments.sh
@@ -9,6 +9,7 @@ ACCELERATIONS="500 1000 1500 2000 2500 3000 4000 5000"
 PROCESSORS="1/0-1,24-25 4/0-4,24-28 8/0-8,24-32 16/0-8,12-19,24-32,36-43"
 MONPOLY_CPU_LIST="0"
 AUX_CPU_LIST="9-10,33-34"
+REPLAYER_QUEUE=1200
 
 VERDICT_FILE="$OUTPUT_DIR/verdicts.txt"
 
@@ -36,7 +37,7 @@ for formula in $FORMULAS; do
             SUMMARY_REPORT="$REPORT_DIR/nokia_monpoly_${formula}_${acc}_${i}_summary.txt"
 
             rm "$VERDICT_FILE"
-            (taskset -c $AUX_CPU_LIST "$WORK_DIR/replayer.sh" -v -a $acc -m "$WORK_DIR/ldcc_sample.csv" 2> "$DELAY_REPORT") \
+            (taskset -c $AUX_CPU_LIST "$WORK_DIR/replayer.sh" -v -a $acc -q $REPLAYER_QUEUE -m "$WORK_DIR/ldcc_sample.csv" 2> "$DELAY_REPORT") \
                 | taskset -c $MONPOLY_CPU_LIST "$TIME_COMMAND" -f "%e %M" -o "$SUMMARY_REPORT" "$WORK_DIR/monpoly" -sig "$WORK_DIR/nokia/ldcc.sig" -formula "$WORK_DIR/nokia/$formula.mfotl" -load "$STATE_FILE" -negate > "$VERDICT_FILE"
         done
     done
@@ -63,7 +64,7 @@ for procs in $PROCESSORS; do
                 DELAY_REPORT="$REPORT_DIR/nokia_flink_${numcpus}_${formula}_${acc}_${i}_delay.txt"
 
                 rm "$VERDICT_FILE"
-                taskset -c $AUX_CPU_LIST "$WORK_DIR/replayer.sh" -v -a $acc -o localhost:$STREAM_PORT "$WORK_DIR/ldcc_sample.csv" 2> "$DELAY_REPORT" &
+                taskset -c $AUX_CPU_LIST "$WORK_DIR/replayer.sh" -v -a $acc -q $REPLAYER_QUEUE -o localhost:$STREAM_PORT "$WORK_DIR/ldcc_sample.csv" 2> "$DELAY_REPORT" &
                 "$WORK_DIR/monitor.sh" --in localhost:$STREAM_PORT --format csv --out "$VERDICT_FILE" --monitor "$WORK_DIR/monpoly -negate -load $STATE_FILE" --sig "$WORK_DIR/nokia/ldcc.sig" --formula "$WORK_DIR/nokia/$formula.mfotl" --processors $numcpus --job "$JOB_NAME"
             done
         done
@@ -94,7 +95,7 @@ for procs in $PROCESSORS; do
                 DELAY_REPORT="$REPORT_DIR/nokia_flink_ft_${numcpus}_${formula}_${acc}_${i}_delay.txt"
 
                 rm "$VERDICT_FILE"
-                taskset -c $AUX_CPU_LIST "$WORK_DIR/replayer.sh" -v -a $acc -o localhost:$STREAM_PORT "$WORK_DIR/ldcc_sample.csv" 2> "$DELAY_REPORT" &
+                taskset -c $AUX_CPU_LIST "$WORK_DIR/replayer.sh" -v -a $acc -q $REPLAYER_QUEUE -o localhost:$STREAM_PORT "$WORK_DIR/ldcc_sample.csv" 2> "$DELAY_REPORT" &
                 "$WORK_DIR/monitor.sh" --checkpoints "file://$CHECKPOINT_DIR" --in localhost:$STREAM_PORT --format csv --out "$VERDICT_FILE" --monitor "$WORK_DIR/monpoly -negate -load $STATE_FILE" --sig "$WORK_DIR/nokia/ldcc.sig" --formula "$WORK_DIR/nokia/$formula.mfotl" --processors $numcpus --job "$JOB_NAME"
             done
         done
