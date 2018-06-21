b57358247aa6c6a6f268ec0f46e9db93a2cd61be
Joshua Schneider <joshua.schneider@inf.ethz.ch>
2018-06-21 10:53:39 +0200

    limit Nokia sample to 24 hours

--- evaluation/nokia/cut_log.py
+++ evaluation/nokia/cut_log.py
@@ -4,8 +4,8 @@ import datetime
 import re
 import sys
 
-main_start = datetime.datetime(2010, 8, 27, 12, 0, 0, tzinfo=datetime.timezone.utc)
-main_end   = datetime.datetime(2010, 8, 30, 12, 0, 0, tzinfo=datetime.timezone.utc)
+main_start = datetime.datetime(2010, 8, 27, 15, 0, 0, tzinfo=datetime.timezone.utc)
+main_end   = datetime.datetime(2010, 8, 28, 15, 0, 0, tzinfo=datetime.timezone.utc)
 past_reach = datetime.timedelta(hours=30)
 
 past_ts = int((main_start - past_reach).timestamp())
