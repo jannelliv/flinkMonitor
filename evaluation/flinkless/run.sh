dd8d0ea12cc1e1fcaf1a531e5a0b47d1920fde60
krle <srdan.krstic@polimi.it>
2019-02-02 00:14:27 +0100

    params

--- evaluation/flinkless/run.sh
+++ evaluation/flinkless/run.sh
@@ -268,8 +268,6 @@ function make_log() {
     strategy=$(echo $strategy | sed s/\"//g)
     "$WORK_DIR/generator.sh" $formula -e $er -i $ir -t $start -w $WINDOW $strategy $length | "$WORK_DIR/replayer.sh" -a 0 -m > $log 
     
-    debug "$WORK_DIR/generator.sh" $formula -e $er -i $ir -t $start -w $WINDOW $strategy $length
-
     echo "${name}"
 }
 
